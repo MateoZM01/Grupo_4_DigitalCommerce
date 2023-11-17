8d5baff2e88495e1b290555c6544f3a284fb0401
Ktawin <145071572+Ktawin146@users.noreply.github.com>
2023-11-17 12:03:16 -0300

    login

---
+++ CONTB/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+
+app.listen(5000, ()=>{
+    console.log('BOCA BOCAAAA ESTO ES BOCAAAA');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
