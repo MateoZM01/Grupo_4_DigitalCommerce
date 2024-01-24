857ce9363ebda88639b7f8e79800b6c5f5e386a8
Mateo <mateozarba01@gmail.com>
2024-01-23 21:22:04 -0300

    reparando detalle del producto

--- src/controllers/productsControllers.js
+++ src/controllers/productsControllers.js
@@ -1,17 +1,18 @@
 // Objeto productsControllers que contiene funciones relacionadas con productos
 const productsControllers = {
-    // Controlador para obtener todos los productos
-    getAllProducts: (req, res) => {
-        // Renderiza la vista llamada "home"
-        res.render("home");
-    },
-    // Controlador para la ruta del detalle del producto ("/productDetail")
-    productDetail: (req, res) => {
-        // Renderiza la vista llamada "productDetail" en la carpeta "products"
-        res.render('productDetail');
-    }
+  // Controlador para obtener todos los productos
+  getAllProducts: (req, res) => {
+    // Renderiza la vista llamada "home"
+    res.render("home");
+  },
+  // Controlador para la ruta del detalle del producto ("/productDetail")
+  productDetail: (req, res) => {
+    // Renderiza la vista llamada "productDetail" en la carpeta "products"
+    res.render("productDetail");
+    const id = req.params.id;
+    console.log(id);
+  },
 };
-
 
 // Exporta el objeto productsControllers para su uso en otros archivos
 module.exports = productsControllers;
