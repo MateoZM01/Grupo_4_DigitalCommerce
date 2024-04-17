f041a106a934ac1a909535f5f9737b0c90b2ce5b
martinguzol <martin_guzman_@hotmail.com>
2024-04-16 22:57:30 -0300

    Agregando carpeta Diagrama_DB y poblando tablas

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   "development": {
     "username": "root",
     "password": "root",
-    "database": "digitalcommerce",
+    "database": "digital_commerce",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
